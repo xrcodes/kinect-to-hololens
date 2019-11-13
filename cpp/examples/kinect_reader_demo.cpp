093609c357ef8f63d81b02dc177eae6ab4708f82
Hanseul Jun <hanseuljun@gmail.com>
2019-11-12 23:53:33 -0800

    Remove Kinect v2 functions from the c++ demos.

--- cpp/examples/kinect_reader_demo.cpp
+++ cpp/examples/kinect_reader_demo.cpp
@@ -6,97 +6,6 @@
 
 namespace kh
 {
-// An obsolete function for Kinect v2.
-void _display_frames()
-{
-    // Obtain KinectDevice to access Kinect frames.
-    auto device = kinect::obtainKinectDevice();
-    if (!device) {
-        std::cout << "Could not find a kinect" << std::endl;
-        return;
-    }
-
-    // The width and height of the Vp8Encoder are the half of Kinect frames'
-    // since frames get halved before getting encoded.
-    Vp8Encoder encoder(960, 540, 2000);
-    Vp8Decoder decoder;
-
-    for (;;) {
-        // Try acquiring a Kinect frame until a valid frame pops up.
-        auto kinect_frame = device->acquireFrame();
-        if (!kinect_frame)
-            continue;
-
-        // Encodes and decodes color pixels just to test whether Vp8Encoder and Vp8Decoder works.
-        // Then, converts the pixels for OpenCV.
-        auto yuv_image = createHalvedYuvImageFromKinectColorBuffer(kinect_frame->color_frame()->getRawUnderlyingBuffer());
-        auto vp8_frame = encoder.encode(yuv_image);
-        auto ffmpeg_frame = decoder.decode(vp8_frame.data(), vp8_frame.size());
-        auto color_mat = createCvMatFromYuvImage(createYuvImageFromAvFrame(ffmpeg_frame.av_frame()));
-
-        // Compresses and decompresses the depth pixels to test the compression and decompression functions.
-        // Then, converts the pixels for OpenCV.
-        auto rvl_frame = createRvlFrameFromKinectDepthBuffer(kinect_frame->depth_frame()->getUnderlyingBuffer());
-        auto depth_image = createDepthImageFromRvlFrame(rvl_frame.data());
-        auto depth_mat = createCvMatFromKinectDepthImage(depth_image.data());
-
-        // Displays the color and depth pixels.
-        cv::imshow("Color", color_mat);
-        cv::imshow("Depth", depth_mat);
-        if (cv::waitKey(1) >= 0)
-            break;
-    }
-}
-
-// An obsolete function for Kinect v2.
-void _display_intrinsics()
-{
-    // Obtain KinectIntrinsics using Freenect2.
-    auto intrinsics = kinect::obtainKinectIntrinsics();
-    if (!intrinsics) {
-        std::cout << "Could not find intrinsics of a Kinect." << std::endl;
-        return;
-    }
-
-    // Print all parameters of KinectIntrinsics.
-    std::cout << "Color fx: " << intrinsics->color_params.fx << std::endl;
-    std::cout << "Color fy: " << intrinsics->color_params.fy << std::endl;
-    std::cout << "Color cx: " << intrinsics->color_params.cx << std::endl;
-    std::cout << "Color cy: " << intrinsics->color_params.cy << std::endl;
-    std::cout << "Color shift_d: " << intrinsics->color_params.shift_d << std::endl;
-    std::cout << "Color shift_m: " << intrinsics->color_params.shift_m << std::endl;
-    std::cout << "Color mx_x3y0: " << intrinsics->color_params.mx_x3y0 << std::endl;
-    std::cout << "Color mx_x0y3: " << intrinsics->color_params.mx_x0y3 << std::endl;
-    std::cout << "Color mx_x2y1: " << intrinsics->color_params.mx_x2y1 << std::endl;
-    std::cout << "Color mx_x1y2: " << intrinsics->color_params.mx_x1y2 << std::endl;
-    std::cout << "Color mx_x2y0: " << intrinsics->color_params.mx_x2y0 << std::endl;
-    std::cout << "Color mx_x0y2: " << intrinsics->color_params.mx_x0y2 << std::endl;
-    std::cout << "Color mx_x1y1: " << intrinsics->color_params.mx_x1y1 << std::endl;
-    std::cout << "Color mx_x1y0: " << intrinsics->color_params.mx_x1y0 << std::endl;
-    std::cout << "Color mx_x0y1: " << intrinsics->color_params.mx_x0y1 << std::endl;
-    std::cout << "Color mx_x0y0: " << intrinsics->color_params.mx_x0y0 << std::endl;
-    std::cout << "Color my_x3y0: " << intrinsics->color_params.my_x3y0 << std::endl;
-    std::cout << "Color my_x0y3: " << intrinsics->color_params.my_x0y3 << std::endl;
-    std::cout << "Color my_x2y1: " << intrinsics->color_params.my_x2y1 << std::endl;
-    std::cout << "Color my_x1y2: " << intrinsics->color_params.my_x1y2 << std::endl;
-    std::cout << "Color my_x2y0: " << intrinsics->color_params.my_x2y0 << std::endl;
-    std::cout << "Color my_x0y2: " << intrinsics->color_params.my_x0y2 << std::endl;
-    std::cout << "Color my_x1y1: " << intrinsics->color_params.my_x1y1 << std::endl;
-    std::cout << "Color my_x1y0: " << intrinsics->color_params.my_x1y0 << std::endl;
-    std::cout << "Color my_x0y1: " << intrinsics->color_params.my_x0y1 << std::endl;
-    std::cout << "Color my_x0y0: " << intrinsics->color_params.my_x0y0 << std::endl;
-
-    std::cout << "IR fx: " << intrinsics->ir_params.fx << std::endl;
-    std::cout << "IR fy: " << intrinsics->ir_params.fy << std::endl;
-    std::cout << "IR cx: " << intrinsics->ir_params.cx << std::endl;
-    std::cout << "IR cy: " << intrinsics->ir_params.cy << std::endl;
-    std::cout << "IR k1: " << intrinsics->ir_params.k1 << std::endl;
-    std::cout << "IR k2: " << intrinsics->ir_params.k2 << std::endl;
-    std::cout << "IR k3: " << intrinsics->ir_params.k3 << std::endl;
-    std::cout << "IR p1: " << intrinsics->ir_params.p1 << std::endl;
-    std::cout << "IR p2: " << intrinsics->ir_params.p2 << std::endl;
-}
-
 void _display_azure_kinect_frames()
 {
     const int TARGET_BITRATE = 2000;
@@ -121,7 +30,6 @@ void _display_azure_kinect_frames()
         return;
     }
 
-    // K4A_COLOR_RESOLUTION_720P has a resolution of 1280x720.
     Vp8Encoder encoder(calibration->color_camera_calibration.resolution_width,
                        calibration->color_camera_calibration.resolution_height,
                        TARGET_BITRATE);
--- cpp/examples/kinect_receiver_demo.cpp
+++ cpp/examples/kinect_receiver_demo.cpp
@@ -6,85 +6,6 @@
 
 namespace kh
 {
-void _receive_frames(std::string ip_address, int port)
-{
-    std::cout << "Try connecting to " << ip_address << ":" << port << std::endl;
-
-    // Try connecting to a Sender with the IP address and the port.
-    asio::io_context io_context;
-    Receiver receiver(io_context);
-    for (;;) {
-        if (receiver.connect(ip_address, port))
-            break;
-    }
-
-    std::cout << "Connected!" << std::endl;
-
-    Vp8Decoder decoder;
-    for (;;) {
-        // Try receiving a message from the Receiver.
-        auto receive_result = receiver.receive();
-        // Keep trying again if there is none.
-        if(!receive_result)
-            continue;
-
-        int cursor = 0;
-
-        auto message_type = (*receive_result)[0];
-        cursor += 1;
-
-        // There can be two types of messages: a KinectIntrinsics and a Kinect frame.
-        if (message_type == 0) {
-            // This application has nothing to do with a KinectIntrinsics that is for 3D rendering
-            // since it uses OpenCV to render the frames in 2D.
-            std::cout << "Received intrinsics." << std::endl;
-        } else if (message_type == 1) {
-            // Parse the ID of the frame and send a feedback meesage to the sender
-            // to indicate the frame was succesfully received.
-            // This is required to minimize the end-to-end latency from the Kinect of the Sender
-            // and the display of the Receiver.
-            int frame_id;
-            memcpy(&frame_id, receive_result->data() + cursor, 4);
-            cursor += 4;
-
-            if(frame_id % 100 == 0)
-                std::cout << "Received frame " << frame_id << "." << std::endl;
-            receiver.send(frame_id);
-
-            // Parsing the bytes of the message into the VP8 and RVL frames.
-            int vp8_frame_size;
-            memcpy(&vp8_frame_size, receive_result->data() + cursor, 4);
-            cursor += 4;
-
-            std::vector<uint8_t> vp8_frame(vp8_frame_size);
-            memcpy(vp8_frame.data(), receive_result->data() + cursor, vp8_frame_size);
-            cursor += vp8_frame_size;
-
-            int rvl_frame_size;
-            memcpy(&rvl_frame_size, receive_result->data() + cursor, 4);
-            cursor += 4;
-
-            std::vector<uint8_t> rvl_frame(rvl_frame_size);
-            memcpy(rvl_frame.data(), receive_result->data() + cursor, rvl_frame_size);
-            cursor += rvl_frame_size;
-
-            // Decoding a Vp8Frame into color pixels.
-            auto ffmpeg_frame = decoder.decode(vp8_frame.data(), vp8_frame.size());
-            auto color_mat = createCvMatFromYuvImage(createYuvImageFromAvFrame(ffmpeg_frame.av_frame()));
-
-            // Decompressing a RVL frame into depth pixels.
-            auto depth_image = createDepthImageFromRvlFrame(rvl_frame.data());
-            auto depth_mat = createCvMatFromKinectDepthImage(depth_image.data());
-
-            // Rendering the depth pixels.
-            cv::imshow("Color", color_mat);
-            cv::imshow("Depth", depth_mat);
-            if (cv::waitKey(1) >= 0)
-                break;
-        }
-    }
-}
-
 void _receive_azure_kinect_frames(std::string ip_address, int port)
 {
     const int AZURE_KINECT_DEPTH_WIDTH = 640;
@@ -185,7 +106,6 @@ void receive_frames()
         // The default port is 7777.
         int port = port_line.empty() ? 7777 : std::stoi(port_line);
         try {
-            //_receive_frames(ip_address, port);
             _receive_azure_kinect_frames(ip_address, port);
         } catch (std::exception& e) {
             std::cout << e.what() << std::endl;
