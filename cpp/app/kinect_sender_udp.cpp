40003c69ea0762b8ea1754ddc48c7917352ab75d
Hanseul Jun <hanseuljun@gmail.com>
2020-02-01 18:19:13 -0800

    Remove KinectPlayer.

--- cpp/app/kinect_sender_udp.cpp
+++ cpp/app/kinect_sender_udp.cpp
@@ -20,21 +20,6 @@ int pow_of_two(int exp) {
     return res;
 }
 
-std::vector<std::string> get_filenames_from_folder_path(std::string folder_path)
-{
-    std::vector<std::string> filenames;
-    for (const auto& entry : std::filesystem::directory_iterator(folder_path)) {
-        std::string filename = entry.path().filename().string();
-        if (filename == ".gitignore")
-            continue;
-        if (entry.is_directory())
-            continue;
-        filenames.push_back(filename);
-    }
-
-    return filenames;
-}
-
 class KinectDevice
 {
 private:
