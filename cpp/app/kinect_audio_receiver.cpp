2895cd2c06dd64c47c348cfadc3a108d329ab27a
Hanseul Jun <hanseuljun@gmail.com>
2020-02-12 02:50:11 -0800

    Call opus_decode_float in a way for packet losses.

--- cpp/app/kinect_audio_receiver.cpp
+++ cpp/app/kinect_audio_receiver.cpp
@@ -123,23 +123,32 @@ int main(std::string ip_address, int port)
             int audio_packet_cursor = 5;
             int frame_id = copy_from_packet<int>(packet_it->second, audio_packet_cursor);
 
-            //if (frame_id <= last_frame_id)
-            //    continue;
+            int frame_size;
+            if (frame_id <= last_frame_id) {
+                // If a packet is about the past, throw it away and try again.
+                packet_it = packets.erase(packet_it);
+                continue;
+            } else if (frame_id == last_frame_id + 1) {
+                // When the packet for the next audio frame is found,
+                // use it and erase it.
+                int opus_frame_size = copy_from_packet<int>(packet_it->second, audio_packet_cursor);
+                frame_size = opus_decode_float(opus_decoder, packet_it->second.data() + audio_packet_cursor, opus_frame_size, out, AUDIO_FRAME_SIZE, 0);
+                packet_it = packets.erase(packet_it);
+            } else {
+                // If not, let opus know there is a packet loss.
+                frame_size = opus_decode_float(opus_decoder, nullptr, 0, out, AUDIO_FRAME_SIZE, 0);
+            }
 
-            int opus_frame_size = copy_from_packet<int>(packet_it->second, audio_packet_cursor);
-
-            int frame_size = opus_decode_float(opus_decoder, packet_it->second.data() + audio_packet_cursor, opus_frame_size, out, AUDIO_FRAME_SIZE, 0);
             if (frame_size < 0) {
                 printf("decoder failed: %s\n", opus_strerror(frame_size));
                 return 1;
             }
 
-            printf("frame_id: %d, opus_frame_size: %d, frame_size: %d\n", frame_id, opus_frame_size, frame_size);
+            //printf("frame_id: %d, opus_frame_size: %d, frame_size: %d\n", frame_id, opus_frame_size, frame_size);
             memcpy(write_ptr + write_cursor, out, FRAME_BYTE_SIZE);
 
-            last_frame_id = frame_id;
+            ++last_frame_id;
             write_cursor += FRAME_BYTE_SIZE;
-            packet_it = packets.erase(packet_it);
         }
 
         soundio_ring_buffer_advance_write_ptr(soundio_helper::ring_buffer, write_cursor);
