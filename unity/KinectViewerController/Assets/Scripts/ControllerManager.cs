69f91152144b8f4c755f684fb2125f40fb1565be
Hanseul Jun <hanseuljun@gmail.com>
2020-04-27 00:33:40 -0700

    Add dear-imgui-unity as submodule and use it for the controller.

--- unity/KinectViewerController/Assets/Scripts/ControllerManager.cs
+++ unity/KinectViewerController/Assets/Scripts/ControllerManager.cs
@@ -1,12 +1,9 @@
 ﻿using System.Net.Sockets;
-using System.Text;
 using UnityEngine;
-using UnityEngine.UI;
+using ImGuiNET;
 
 public class ControllerManager : MonoBehaviour
 {
-    public Text viewerStateText;
-    public InputField kinectAddressInputField;
     private TcpSocket tcpSocket;
     private MessageBuffer messageBuffer;
     private TcpSocket remoteSocket;
@@ -34,19 +31,19 @@ public class ControllerManager : MonoBehaviour
             print(e.Message);
         }
 
-        if (this.remoteSocket != null)
-        {
-            byte[] message;
-            while (messageBuffer.TryReceiveMessage(this.remoteSocket, out message))
-            {
-                var viewerStateJson = Encoding.ASCII.GetString(message);
-                var viewerState = JsonUtility.FromJson<ViewerState>(viewerStateJson);
+        //if (this.remoteSocket != null)
+        //{
+        //    byte[] message;
+        //    while (messageBuffer.TryReceiveMessage(this.remoteSocket, out message))
+        //    {
+        //        var viewerStateJson = Encoding.ASCII.GetString(message);
+        //        var viewerState = JsonUtility.FromJson<ViewerState>(viewerStateJson);
 
-                //print($"this.remoteSocket.Socket.RemoteEndPoint: {this.remoteSocket.Socket.RemoteEndPoint}");
+        //        //print($"this.remoteSocket.Socket.RemoteEndPoint: {this.remoteSocket.Socket.RemoteEndPoint}");
 
-                viewerStateText.text = $"end point: {this.remoteSocket.Socket.RemoteEndPoint}\nuser ID: {viewerState.userId}";
-            }
-        }
+        //        viewerStateText.text = $"end point: {this.remoteSocket.Socket.RemoteEndPoint}\nuser ID: {viewerState.userId}";
+        //    }
+        //}
     }
 
     void OnApplicationQuit()
@@ -56,6 +53,22 @@ public class ControllerManager : MonoBehaviour
 
     public void OnConnect()
     {
-        print($"kinectAddressInputField: {kinectAddressInputField.text}");
+        //print($"kinectAddressInputField: {kinectAddressInputField.text}");
+    }
+
+    void OnEnable()
+    {
+        ImGuiUn.Layout += OnLayout;
+    }
+
+    void OnDisable()
+    {
+        ImGuiUn.Layout -= OnLayout;
+    }
+
+    void OnLayout()
+    {
+        print("OnLayout");
+        ImGui.ShowDemoWindow();
     }
 }
