bac1c857ea6a93a34330c0a527c6151c2c14d9ba
Hanseul Jun <hanseuljun@gmail.com>
2020-04-28 16:59:46 -0700

    See a working version of the geometry shader in unity editor.

--- unity/KinectViewer/Assets/Graphics/KinectScreen.shader
+++ unity/KinectViewer/Assets/Graphics/KinectScreen.shader
@@ -17,6 +17,7 @@
             CGPROGRAM
             #pragma target 4.0
             #pragma vertex vert
+            #pragma geometry geom
             #pragma fragment frag
 
             #include "UnityCG.cginc"
@@ -25,42 +26,93 @@
             {
                 float3 vertex : POSITION;
                 float2 uv : TEXCOORD0;
-
-                UNITY_VERTEX_INPUT_INSTANCE_ID
+                float2 size : TEXCOORD1;
             };
 
-            struct v2f
+            struct v2g
+            {
+                fixed4 vertex : POSITION;
+                fixed2 uv : TEXCOORD0;
+                fixed2 vertex_offset : TEXCOORD1;
+
+                //UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct g2f
             {
                 float4 vertex : SV_POSITION;
                 float2 uv : TEXCOORD0;
 
-                UNITY_VERTEX_OUTPUT_STEREO
+                //UNITY_VERTEX_OUTPUT_STEREO
             };
 
             Texture2D _YTex;
             Texture2D _UvTex;
             SamplerState sampler_YTex;
             sampler2D _DepthTex;
+            float4x4 _ModelMatrix;
+            fixed4 _VertexOffsetXVector;
+            fixed4 _VertexOffsetYVector;
 
-            v2f vert (appdata v)
+            v2g vert (appdata v)
             {
-                v2f o;
+                v2g o;
 
-                UNITY_SETUP_INSTANCE_ID(v);
-                UNITY_INITIALIZE_OUTPUT(v2f, o);
-                UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(o);
+                //UNITY_SETUP_INSTANCE_ID(v);
+                //UNITY_INITIALIZE_OUTPUT(v2f, o);
+                //UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(o);
 
                 // 65.535 is equivalent to (2^16 - 1) / 1000, where (2^16 - 1) is to complement
                 // the conversion happened in the texture-level from 0 ~ (2^16 - 1) to 0 ~ 1.
                 // 1000 is the conversion of mm (the unit of Azure Kinect) to m (the unit of Unity3D).
                 fixed depth = tex2Dlod(_DepthTex, fixed4(v.uv, 0, 0)).r * 65.535;
-                o.vertex = UnityObjectToClipPos(v.vertex * depth);
+                
+                o.vertex = float4(v.vertex * depth, 1.0);
                 o.uv = v.uv;
+                o.vertex_offset = v.size * depth;
 
                 return o;
             }
 
-            fixed4 frag(v2f i) : SV_Target
+            [maxvertexcount(4)]
+            void geom(point v2g i[1], inout TriangleStream<g2f> triangles)
+            {
+                // Filtering out invalid depth pixels.
+                if (i[0].vertex.z > 0.1)
+                {
+                    g2f o;
+                    // Tried using mvp matrix instead of the below one using vp matrix and unity_ObjectToWorld.
+                    // It turns calculating vertex, offset_x, and offset_y from 6 matrix-vector multiplications into
+                    // a matrix-matrix multiplication and 3 matrix-vector multiplications.
+                    // Unfortunately, it didn't improved this shader...
+                    
+                    // Using _ModelMatrix since UNITY_MATRIX_M is not working inside the geometry shader.
+                    // Seems like it is an identity matrix, especially for the universal rendering pipeline.
+                    // If Unity fixes this bug, use UNITY_MATRIX_M instead of _ModelMatrix.
+                    fixed4 vertex = mul(UNITY_MATRIX_VP, mul(_ModelMatrix, i[0].vertex));
+                    fixed4 offset_x = mul(UNITY_MATRIX_VP, mul(_ModelMatrix, _VertexOffsetXVector)) * i[0].vertex_offset.x;
+                    fixed4 offset_y = mul(UNITY_MATRIX_VP, mul(_ModelMatrix, _VertexOffsetYVector)) * i[0].vertex_offset.y;
+
+                    o.vertex = vertex - offset_x * 0.5 - offset_y * 0.5;
+
+                    // This does not optimize code since the code above already was converted to mad by the optimizer.
+                    //o.vertex = mad(offset_x + offset_y, -0.5, vertex);
+
+                    o.uv = i[0].uv;
+                    triangles.Append(o);
+
+                    o.vertex = o.vertex + offset_x;
+                    triangles.Append(o);
+
+                    o.vertex = o.vertex - offset_x + offset_y;
+                    triangles.Append(o);
+
+                    o.vertex = o.vertex + offset_x;
+                    triangles.Append(o);
+                }
+            }
+
+            fixed4 frag(g2f i) : SV_Target
             {
                 // Formula came from https://docs.microsoft.com/en-us/windows/desktop/medfound/recommended-8-bit-yuv-formats-for-video-rendering.
                 // Based on results from the profiler, conversion does not take that much time.
--- unity/KinectViewer/Assets/Scripts/KinectScreen.cs
+++ unity/KinectViewer/Assets/Scripts/KinectScreen.cs
@@ -11,6 +11,56 @@ public class KinectScreen : MonoBehaviour
 
     public Material Material => meshRenderer.sharedMaterial;
 
+    void Awake()
+    {
+        RenderPipelineManager.beginCameraRendering += OnBeginCameraRendering;
+    }
+
+    void OnDestroy()
+    {
+        RenderPipelineManager.beginCameraRendering -= OnBeginCameraRendering;
+    }
+
+    void OnBeginCameraRendering(ScriptableRenderContext context, Camera camera)
+    {
+        // Ignore when it is called from Unity's "Scene" (not "Game").
+        if (camera.cameraType == CameraType.SceneView)
+            return;
+
+        // Using _ModelMatrix since UNITY_MATRIX_M is not working inside the geometry shader.
+        // Seems like it is an identity matrix, especially for the universal rendering pipeline.
+        // If Unity fixes this bug, use UNITY_MATRIX_M instead of _ModelMatrix.
+        meshRenderer.sharedMaterial.SetMatrix("_ModelMatrix", transform.localToWorldMatrix);
+
+        var cameraTransform = camera.transform;
+        var worldCameraFrontVector = cameraTransform.TransformDirection(new Vector3(0.0f, 0.0f, 1.0f));
+
+        // Using the y direction as the up vector instead the up vector of the camera allows the user to feel more
+        // comfortable as it preserves the sense of gravity.
+        // Getting the right vector directly from the camera transform through zeroing its y-component does not
+        // work when the y-component of the camera's up vector is negative. While it is possible to solve the problem
+        // with an if statement, inverting when the y-component is negative, I decided to detour this case with
+        // usage of the cross product with the front vector.
+        var worldUpVector = new Vector3(0.0f, 1.0f, 0.0f);
+        var worldRightVector = Vector3.Cross(worldUpVector, worldCameraFrontVector);
+        worldRightVector = new Vector3(worldRightVector.x, 0.0f, worldRightVector.z);
+        worldRightVector.Normalize();
+
+        var localRightVector = transform.InverseTransformDirection(worldRightVector);
+        var localUpVector = transform.InverseTransformDirection(worldUpVector);
+
+        // The coordinate system of Kinect's textures have (1) its origin at its left-up side.
+        // Also, the viewpoint of it is sort of (2) the opposite of the viewpoint of the Hololens, considering the typical use case. (this is not the case for Azure Kinect)
+        // Due to (1), vertexOffsetYVector = -localCameraUpVector.
+        // Due to (2), vertexOffsetXVector = -localCameraRightVector.
+        //var vertexOffsetXVector = -localRightVector;
+        var vertexOffsetXVector = localRightVector;
+        var vertexOffsetYVector = -localUpVector;
+
+        meshRenderer.sharedMaterial.SetVector("_VertexOffsetXVector", new Vector4(vertexOffsetXVector.x, vertexOffsetXVector.y, vertexOffsetXVector.z, 0.0f));
+        meshRenderer.sharedMaterial.SetVector("_VertexOffsetYVector", new Vector4(vertexOffsetYVector.x, vertexOffsetYVector.y, vertexOffsetYVector.z, 0.0f));
+    }
+
     // Since calculation including Unproject() takes too much time,
     // this function is made to run as a coroutine that takes a break
     // every 100 ms.
@@ -49,46 +99,27 @@ public class KinectScreen : MonoBehaviour
             }
         }
 
-        const float QUAD_AMPLIFIER = 1.2f;
-        const float TWO_MINUS_QUAD_AMPLIFIER = 2.0f - QUAD_AMPLIFIER;
         int quadWidth = width - 2;
         int quadHeight = height - 2;
-        var quadVertices = new Vector3[quadWidth * quadHeight * 4];
-        var quadUv = new Vector2[quadWidth * quadHeight * 4];
+        var quadVertices = new Vector3[quadWidth * quadHeight];
+        var quadUv = new Vector2[quadWidth * quadHeight];
+        var quadSizes = new Vector2[quadWidth * quadHeight];
 
         for (int ii = 0; ii < quadWidth; ++ii)
         {
             for (int jj = 0; jj < quadHeight; ++jj)
             {
-                int quadIndex = ii + jj * quadWidth;
                 int i = ii + 1;
                 int j = jj + 1;
-                //quadVertices[quadIndex * 4 + 0] = (vertices[i + j * width] + vertices[(i - 1) + (j - 1) * width]) * 0.5f;
-                //quadVertices[quadIndex * 4 + 1] = (vertices[i + j * width] + vertices[(i + 1) + (j - 1) * width]) * 0.5f;
-                //quadVertices[quadIndex * 4 + 2] = (vertices[i + j * width] + vertices[(i - 1) + (j + 1) * width]) * 0.5f;
-                //quadVertices[quadIndex * 4 + 3] = (vertices[i + j * width] + vertices[(i + 1) + (j + 1) * width]) * 0.5f;
-                quadVertices[quadIndex * 4 + 0] = (vertices[i + j * width] * TWO_MINUS_QUAD_AMPLIFIER + vertices[(i - 1) + (j - 1) * width] * QUAD_AMPLIFIER) * 0.5f;
-                quadVertices[quadIndex * 4 + 1] = (vertices[i + j * width] * TWO_MINUS_QUAD_AMPLIFIER + vertices[(i + 1) + (j - 1) * width] * QUAD_AMPLIFIER) * 0.5f;
-                quadVertices[quadIndex * 4 + 2] = (vertices[i + j * width] * TWO_MINUS_QUAD_AMPLIFIER + vertices[(i - 1) + (j + 1) * width] * QUAD_AMPLIFIER) * 0.5f;
-                quadVertices[quadIndex * 4 + 3] = (vertices[i + j * width] * TWO_MINUS_QUAD_AMPLIFIER + vertices[(i + 1) + (j + 1) * width] * QUAD_AMPLIFIER) * 0.5f;
-
-                quadUv[quadIndex * 4 + 0] = uv[i + j * width];
-                quadUv[quadIndex * 4 + 1] = uv[i + j * width];
-                quadUv[quadIndex * 4 + 2] = uv[i + j * width];
-                quadUv[quadIndex * 4 + 3] = uv[i + j * width];
+                quadVertices[ii + jj * quadWidth] = vertices[i + j * width];
+                quadUv[ii + jj * quadWidth] = uv[i + j * width];
+                quadSizes[ii + jj * quadWidth] = (vertices[(i + 1) + (j + 1) * width] - vertices[(i - 1) + (j - 1) * width]) * 0.5f;
             }
         }
 
-        var triangles = new int[quadWidth * quadHeight * 6];
+        var triangles = new int[quadWidth * quadHeight];
         for (int i = 0; i < quadWidth * quadHeight; ++i)
-        {
-            triangles[i * 6 + 0] = i * 4 + 0;
-            triangles[i * 6 + 1] = i * 4 + 1;
-            triangles[i * 6 + 2] = i * 4 + 2;
-            triangles[i * 6 + 3] = i * 4 + 1;
-            triangles[i * 6 + 4] = i * 4 + 3;
-            triangles[i * 6 + 5] = i * 4 + 2;
-        }
+            triangles[i] = i;
 
         // Without the bounds, Unity decides whether to render this mesh or not based on the vertices calculated here.
         // This causes Unity not rendering the mesh transformed by the depth texture even when the transformed one
@@ -100,9 +131,10 @@ public class KinectScreen : MonoBehaviour
             indexFormat = IndexFormat.UInt32,
             vertices = quadVertices,
             uv = quadUv,
+            uv2 = quadSizes,
             bounds = bounds,
         };
-        mesh.SetIndices(triangles, MeshTopology.Triangles, 0);
+        mesh.SetIndices(triangles, MeshTopology.Points, 0);
 
         meshFilter.mesh = mesh;
     }
