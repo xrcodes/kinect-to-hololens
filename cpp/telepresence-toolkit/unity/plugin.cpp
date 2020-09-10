2817d2ee6ddfd544c8d90d2ab537e2648945c10b
Hanseul Jun <hanseuljun@gmail.com>
2020-09-09 21:42:48 -0700

    Rename plugin.cpp to graphics.cpp and kh_plugin.cpp to plugin.cpp.

--- cpp/telepresence-toolkit/unity/plugin.cpp
+++ cpp/telepresence-toolkit/unity/plugin.cpp
@@ -1,99 +1,84 @@
-#include <string>
-#define NOMINMAX
-#include <d3d11.h>
-#undef NOMINMAX
-#include "interfaces/IUnityGraphics.h"
-#include "interfaces/IUnityGraphicsD3D11.h"
-#include "texture_group.h"
+#include <opus/opus.h>
+#include "interfaces/IUnityInterface.h"
+#include "core/kh_core.h"
 
-// Unity and Direct3D varaibles that are saved for future use that is mainly through OnRenderEvent().
-static IUnityInterfaces* unity_interfaces_ = nullptr;
-static IUnityGraphics* unity_graphics_ = nullptr;
-static ID3D11Device* d3d11_device_ = nullptr;
-static ID3D11DeviceContext* d3d11_device_context_ = nullptr;
-
-// A callback function for UnityGfxDeviceEvents.
-#pragma warning(push)
-#pragma warning(disable: 26812)
-static void UNITY_INTERFACE_API OnGraphicsDeviceEvent(UnityGfxDeviceEventType eventType)
-#pragma warning(pop)
+// External functions for Unity C# scripts.
+//"C" VoidPtr UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API create_vp8_decoder()
+extern "C"
 {
-    switch(eventType) {
-    case kUnityGfxDeviceEventInitialize: {
-        IUnityGraphicsD3D11* d3d = unity_interfaces_->Get<IUnityGraphicsD3D11>();
-		d3d11_device_ = d3d->GetDevice();
-		// Has to have this if statement since including audio spatializer invokes OnGraphicsDeviceEvent
-		// with kUnityGfxDeviceEventInitialize when d3d->GetDevice() returns null.
-		// I guess that this function gets invoked twice when the audio spatializer is turned on,
-        // and d3d->GetDevice() returns null during the first time, but not the second time.
-		if (d3d11_device_)
-			d3d11_device_->GetImmediateContext(&d3d11_device_context_);
-        break;
+    UNITY_INTERFACE_EXPORT kh::Vp8Decoder* UNITY_INTERFACE_API create_vp8_decoder()
+    {
+        return new kh::Vp8Decoder;
     }
-    case kUnityGfxDeviceEventShutdown: {
-        d3d11_device_ = nullptr;
-        d3d11_device_context_ = nullptr;
-        break;
+
+    UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API delete_vp8_decoder(kh::Vp8Decoder* ptr)
+    {
+        delete ptr;
     }
-    };
-}
 
-// Gets called during a render thread through GL.IssuePluginEvent() in Unity scripts.
-static void UNITY_INTERFACE_API OnRenderEvent(int eventID)
-{
-    constexpr int COMMAND_COUNT{2};
-    const int command_id{eventID % COMMAND_COUNT};
-    const int texture_group_id{eventID / COMMAND_COUNT};
-	switch (command_id) {
-	case 0:
-		texture_group_init(texture_group_id, d3d11_device_);
-		break;
-	case 1:
-		texture_group_update(texture_group_id, d3d11_device_, d3d11_device_context_);
-		break;
-	}
-}
+    UNITY_INTERFACE_EXPORT kh::FFmpegFrame* UNITY_INTERFACE_API vp8_decoder_decode
+    (
+        kh::Vp8Decoder* decoder,
+        std::byte* frame_data,
+        int frame_size
+    )
+    {
+        return new kh::FFmpegFrame(std::move(decoder->decode({frame_data, gsl::narrow_cast<size_t>(frame_size)})));
+    }
 
-// A function that gets called by Unity when the application using this plugin starts.
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces* interfaces)
-{
-    unity_interfaces_ = interfaces;
-    unity_graphics_ = unity_interfaces_->Get<IUnityGraphics>();
-    unity_graphics_->RegisterDeviceEventCallback(OnGraphicsDeviceEvent);
+    UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API delete_ffmpeg_frame(kh::FFmpegFrame* ptr)
+    {
+        delete ptr;
+    }
 
-    // OnGraphicsDeviceEvent() manually called since the editor of Unity may
-    // call OnGraphicsDeviceEvent() only for the first time the scene gets played,
-    // not every time.
-    // For HoloLens, 64-bit means Unity editor since current Unity editors only support 64-bit,
-    // while HoloLens runs on a 32-bit OS.
-#ifdef _WIN64
-    OnGraphicsDeviceEvent(kUnityGfxDeviceEventInitialize);
-#endif
-}
+    UNITY_INTERFACE_EXPORT kh::TrvlDecoder* UNITY_INTERFACE_API create_trvl_decoder(int frame_size)
+    {
+        return new kh::TrvlDecoder(frame_size);
+    }
 
-// A function that Unity calls when the application using this plugin terminates.
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload()
-{
-    unity_graphics_->UnregisterDeviceEventCallback(OnGraphicsDeviceEvent);
-}
+    UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API delete_trvl_decoder(kh::TrvlDecoder* ptr)
+    {
+        delete ptr;
+    }
 
-extern "C" bool UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API has_unity_interfaces()
-{
-	return unity_interfaces_ != nullptr;
-}
+    UNITY_INTERFACE_EXPORT std::vector<std::int16_t>* UNITY_INTERFACE_API trvl_decoder_decode
+    (
+        kh::TrvlDecoder* decoder,
+        std::byte* frame_data,
+        int frame_size,
+        bool keyframe
+    )
+    {
+        return new std::vector<std::int16_t>(std::move(decoder->decode({frame_data, gsl::narrow_cast<size_t>(frame_size)}, keyframe)));
+    }
 
-extern "C" bool UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API has_unity_graphics()
-{
-	return unity_graphics_ != nullptr;
-}
+    UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API delete_depth_pixels(std::vector<int16_t>* ptr)
+    {
+        delete ptr;
+    }
 
-extern "C" bool UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API has_d3d11_device()
-{
-	return d3d11_device_ != nullptr;
-}
+    UNITY_INTERFACE_EXPORT kh::AudioDecoder* UNITY_INTERFACE_API create_audio_decoder(int sample_rate, int channel_count)
+    {
+        return new kh::AudioDecoder(sample_rate, channel_count);
+    }
 
-// A getter function that allows Unity to call OnRenderEvent() in a render thread.
-extern "C" UnityRenderingEvent UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API get_render_event_function_pointer()
-{
-    return OnRenderEvent;
+    UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API destroy_audio_decoder(kh::AudioDecoder* ptr)
+    {
+        delete ptr;
+    }
+
+    UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API audio_decoder_decode
+    (
+        kh::AudioDecoder* decoder,
+        std::byte* opus_frame_data,
+        int opus_frame_size,
+        float* pcm_data,
+        int frame_size
+    )
+    {
+        if(opus_frame_data)
+            return decoder->decode(gsl::span<std::byte>{opus_frame_data, gsl::narrow_cast<size_t>(opus_frame_size)}, pcm_data, frame_size, 0);
+        else
+            return decoder->decode(std::nullopt, pcm_data, frame_size, 0);
+    }
 }
