8b7223c2e75a115f4408c174b77033a46f982b68
Hanseul Jun <hanseuljun@gmail.com>
2020-09-15 19:06:01 -0700

    Add time.cpp and profiler.cpp.

--- cpp/src/native/profiler.h
+++ cpp/src/native/profiler.h
@@ -1,61 +1,22 @@
 #pragma once
 
-#include <unordered_map>
-#include "native/kh_native.h"
+#include <map>
+#include "core/tt_core.h"
 
 namespace kh
 {
 class Profiler
 {
 public:
-    Profiler()
-        : time_point_{tt::TimePoint::now()}, numbers_ {}
-    {
-    }
-
-    void reset()
-    {
-        time_point_ = tt::TimePoint::now();
-        numbers_.clear();
-    }
-
-    void setNumber(std::string name, float number)
-    {
-        auto find_result{numbers_.find(name)};
-        if (find_result == numbers_.end()) {
-            numbers_.insert({name, number});
-        } else {
-            find_result->second = number;
-        }
-    }
-
-    void addNumber(std::string name, float number)
-    {
-        auto find_result{numbers_.find(name)};
-        if (find_result == numbers_.end()) {
-            numbers_.insert({name, number});
-        } else {
-            find_result->second += number;
-        }
-    }
-
-    tt::TimeDuration getElapsedTime()
-    {
-        return time_point_.elapsed_time();
-    }
-
-    float getNumber(std::string name)
-    {
-        auto find_result{numbers_.find(name)};
-        if (find_result == numbers_.end()) {
-            return 0.0f;
-        } else {
-            return find_result->second;
-        }
-    }
+    Profiler();
+    void reset();
+    void setNumber(std::string name, float number);
+    void addNumber(std::string name, float number);
+    tt::TimeDuration getElapsedTime();
+    float getNumber(std::string name);
 
 private:
     tt::TimePoint time_point_;
-    std::unordered_map<std::string, float> numbers_;
+    std::map<std::string, float> numbers_;
 };
 }
